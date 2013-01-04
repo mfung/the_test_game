6828e6dfa2e7e12bc130eb13bea3861a9ba9e070
Meng Fung <m.fung@exteres.com>
2013-01-04 10:21:59 -0800

    Added error checking and refactored all test passed.

--- models/position.rb
+++ models/position.rb
@@ -1,5 +1,17 @@
 class Position
-  def initialize(position)
-    position
+  attr_accessor :cordinate
+  def initialize(cordinate)
+    @cordinate = cordinate
+    raise ArgumentError unless is_valid_cordinate? @cordinate
+  end
+
+  def ==(other_object)
+    other_object.is_a?(Array) ? @cordinate == other_object : super 
+  end
+
+  protected
+  def is_valid_cordinate?(cordinate)
+    cordinate = [cordinate] unless cordinate.is_a? Array
+    cordinate.size === 2 && cordinate.all? { |p| p.is_a? Integer }
   end
 end
